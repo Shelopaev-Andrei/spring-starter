963add1fb227af0f4cfdbdf345ececb336a3fef0
Shelopaev-Andrei <shelopaev.andrei@gmail.com>
2024-06-17 17:19:54 +0300

    Start commit

--- src/main/java/by/demdv/springstarter/SpringStarterApplication.java
+++ src/main/java/by/demdv/springstarter/SpringStarterApplication.java
@@ -8,6 +8,7 @@ public class SpringStarterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringStarterApplication.class, args);
+        System.out.println("Hello");
     }
 
 }
